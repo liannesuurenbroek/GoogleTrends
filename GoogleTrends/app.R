cb18c79fe4eca70f7b85386c1ff9a84e91904bc0
liannesuurenbroek <39589187+liannesuurenbroek@users.noreply.github.com>
2022-04-28 22:58:25 +0200

    Add files via upload

--- GoogleTrends/app.R
+++ GoogleTrends/app.R
@@ -1,50 +1,66 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-
+library(tidyverse)
+library(gtrendsR)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+    titlePanel("Google Trends"),
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            helpText("Choose a term for which you want to know the trends on google"),
+            textInput("text", h3("Search term"), 
+                      value = "R"),
+            actionButton("search", "Search")
         ),
-
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            textOutput("selected_term"),
+            plotOutput("selected_plot")
         )
     )
 )
 
+geenmineen <- function(plottie){
+    if(sum(plottie$hits == "<1")>0){
+        plottie[plottie$hits == "<1",]$hits <- "0"
+        return(plottie)}
+    else{return(plottie)}
+}
+
+geennull <- function(zoekterm){
+    if(typeof(gtrends(zoekterm)$interest_over_time)=="list"){
+        paste("Hits for the search term", zoekterm)
+    }
+    else{
+        paste("This search term could not be found")
+    }
+}
+
+geennullplot <- function(plottie){
+    if(typeof(plottie)=="list"){
+        return(plottie)
+    }
+    else{
+        template=gtrends("R")$interest_over_time
+        template$hits=0
+        return(template)
+    }
+}
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    tekstje <- eventReactive(input$search, {geennull(input$text)})
+    output$selected_term <- renderText({
+        tekstje()
+    })
+    plotje <- eventReactive(input$search,{geenmineen(geennullplot(gtrends(input$text)$interest_over_time))})
+    output$selected_plot <- renderPlot({
+        ggplot(plotje(), aes(x=date, y=as.numeric(hits))) +
+            geom_line() +
+            geom_smooth(span=.3) +
+            scale_x_datetime(name = "Date") +
+            coord_cartesian(ylim = c(0, 100)) +
+            scale_y_continuous(name = "Relative popularity")
     })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-deployApp()
